13fb88839273f7f29c904a02d5b403cc1a9ccfd7
Faridrhmanzzz <faridrahmanfadhilah.xiia7@gmail.com>
2026-04-18 18:47:47 +0700

    setup app, file and folder structure, routing and auth page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Toaster } from "@/components/ui/sonner"
 import type { Metadata } from "next";
 import { Mona_Sans, Geist } from "next/font/google";
 import "./globals.css";
@@ -22,11 +23,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={cn("dark", "font-sans", geist.variable)}>
-      <body
-        className={`${monaSans.className} antialiased`}
-      >
-        {children}
-      </body>
+          <body
+            className={`${monaSans.className} antialiased pattern`}
+          >
+            {children}
+
+          <Toaster/>
+          </body>
     </html>
   );
 }
